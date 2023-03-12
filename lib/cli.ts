7a339df4a4a7039861f7d13a319ca65777575d9a
E0SelmY4V-PC <2507917746@qq.com>
2023-03-12 22:36:36 +0800

    Add cli@1.0.0

--- lib/cli.ts
+++ lib/cli.ts
@@ -1,9 +1,18 @@
 /**
  * 命令行工具模块
  * @module mcdjs/lib/cli
- * @version 0.9.0
+ * @version 1.0.0
  * @license GPL-3.0-or-later
  */
 declare module './cli';
 
-export { };
+import run, { out, RunInfos } from './hfile';
+import { env } from './config';
+
+export default async function handle() {
+	const input = process.argv.at(-2);
+	const outfile = process.argv.at(-1);
+	if (!input || !outfile) return console.log(`Version ${env.version}`);
+	const infos = new RunInfos({ inputs: [input], outfile });
+	await out(infos, await run(infos));
+}
