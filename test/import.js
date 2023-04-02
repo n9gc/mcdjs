e0409c534539541dfc56d786a84599689e832d03
E0SelmY4V-PC <2507917746@qq.com>
2023-04-02 18:12:36 +0800

    Env 修复ts测试

--- test/import.js
+++ test/import.js
@@ -1,7 +1,20 @@
+const path = require('path');
+const root = path.join(__dirname, '../');
+const file = path.join(root, 'build/index.js');
+const tsc = require('../tsconfig.json').compilerOptions;
+const skipKey = [
+	'outDir',
+	'noImplicitAny',
+	'strict',
+];
+const cmd = ['', ...Object.keys(tsc)].reduce((p, k) =>
+	skipKey.includes(k) ? p : `${p} --${k} ${tsc[k] === true ? '' : tsc[k]}`
+);
 require('export-tester')(
 	{
-		file: require('path').join(__dirname, '../build/index.js'),
+		file,
 		sign: 'McdJS',
+		cfg: { ts: { cmd } },
 	},
 	{
 		import() {
