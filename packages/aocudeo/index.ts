d0a15c15ef0f17daf9261869c62b1b0cdf1c8cb0
r-bsf <132225525+r-bsf@users.noreply.github.com>
2023-05-05 17:30:41 +0000

    Add aocudeo 更好的钩子表示方法

--- packages/aocudeo/index.ts
+++ packages/aocudeo/index.ts
@@ -1,7 +1,7 @@
 /**
  * 胡乱加载链表类定义模块
  * @module aocudeo
- * @version 2.3.1
+ * @version 2.4.0
  * @license GPL-3.0-or-later
  */
 declare module '.';
@@ -9,17 +9,23 @@ declare module '.';
 type AnyArr<T = any> = readonly T[];
 type MayArr<T> = AnyArr<T> | T;
 type Cb<T> = (n: T) => T;
-type Id = string | symbol | number;
+type Hokab = string | number;
+type Id = symbol | Hokab;
+import MapObj = Loader.MapObj;
+import PosInfo = Loader.PosInfo;
 namespace Loader {
 	export interface PosInfo {
 		after?: MayArr<Id>;
 		before?: MayArr<Id>;
+		preOf?: MayArr<Hokab>;
+		postOf?: MayArr<Hokab>;
 	}
+	export type MapObj<T, K extends Id = Id> = { [I in K]: T };
 }
 class Loader<T = void> {
-	static EXIST = Symbol('exist');
-	static noMulti(arr: MayArr<Id> = [], rslt: Id[] = []) {
-		const map: { [x: Id]: symbol; } = {};
+	protected static EXIST = Symbol('exist');
+	protected static noMulti<T extends Id>(arr: MayArr<T> = [], rslt: T[] = []) {
+		const map: MapObj<symbol> = {};
 		rslt.forEach(n => map[n] = this.EXIST);
 		(typeof arr === 'object' ? arr : [arr])
 			.forEach(n => map[n] === this.EXIST
@@ -29,54 +35,68 @@ class Loader<T = void> {
 	}
 	static START = Symbol('load start');
 	static END = Symbol('load end');
+	static HOOK_NAME = ['pre:', 'post:'];
 	constructor(n?: T) {
 		this.n = n!;
 		this.countMap[Loader.START] = 1;
 	}
 	private n: T;
-	private actMap: { [id: Id]: Cb<T>[]; } = Object.create(null);
-	private postListMap: { [id: Id]: Id[]; } = Object.create(null);
-	private countMap: { [id: Id]: number; } = Object.create(null);
+	private actMap: MapObj<Cb<T>[]> = Object.create(null);
+	private postListMap: MapObj<Id[]> = Object.create(null);
+	private countMap: MapObj<number> = Object.create(null);
 	protected getList(id: Id) {
 		return this.postListMap[id] || (this.postListMap[id] = []);
 	}
 	protected plusCount(id: Id, num = 1) {
 		this.countMap[id] = (this.countMap[id] || 0) + num;
 	}
-	protected regAfter(id: Id, pos: Loader.PosInfo) {
-		const afters = Loader.noMulti(pos.after, [Loader.START]);
-		this.plusCount(id, afters.length);
-		afters.forEach(n => this.getList(n).push(id));
+	protected tidy(odName: 'after' | 'before', pos: PosInfo) {
+		const order = odName === 'after';
+		const odSign = Loader.HOOK_NAME[Number(order)];
+		const odUnsign = Loader.HOOK_NAME[Number(!order)];
+		const aous = (n: Hokab) => odUnsign + n;
+		let preOf = Loader.noMulti(pos.preOf);
+		let postOf = Loader.noMulti(pos.postOf);
+		order ? preOf = preOf.map(aous) : postOf = postOf.map(aous);
+		return [
+			...preOf,
+			...Loader.noMulti(pos[odName], [order ? Loader.START : Loader.END])
+				.map(n => typeof n === 'symbol' ? n : odSign + n),
+			...postOf,
+		];
 	}
-	protected regBefore(id: Id, pos: Loader.PosInfo) {
-		const befores = Loader.noMulti(pos.before, [Loader.END]);
+	protected regAfter(id: Id, afters: Id[], lazy = false) {
+		this.plusCount(id, afters.length);
+		afters.forEach(n => this.getList(n)[lazy ? 'unshift' : 'push'](id));
+	}
+	protected regBefore(id: Id, befores: Id[], lazy = false) {
 		befores.forEach(n => this.plusCount(n));
-		this.getList(id).push(...befores);
+		this.getList(id)[lazy ? 'unshift' : 'push'](...befores);
 	}
 	addAct(id: Id, act: MayArr<Cb<T>>) {
 		(this.actMap[id] || (this.actMap[id] = []))
 			.push(...(typeof act === 'function' ? [act] : act));
 		return this;
 	}
-	insert(id: Id, pos: Loader.PosInfo = {}, act: MayArr<Cb<T>> | null = null) {
+	insert(id: Id, pos: PosInfo = {}, act: MayArr<Cb<T>> | null = null) {
 		act && this.addAct(id, act);
 		if (typeof id === 'symbol') {
-			this.regAfter(id, pos);
-			this.regBefore(id, pos);
+			this.regAfter(id, this.tidy('after', pos));
+			this.regBefore(id, this.tidy('before', pos));
 		} else {
-			const preId = `pre:${id}`;
-			const postId = `post:${id}`;
-			this.regAfter(preId, pos);
-			this.regAfter(id, { after: preId });
-			this.regBefore(id, { before: postId });
-			this.regBefore(postId, pos);
+			const preId = Loader.HOOK_NAME[0] + id;
+			const postId = Loader.HOOK_NAME[1] + id;
+			this.regAfter(preId, this.tidy('after', pos));
+			this.regAfter(id, [preId], true);
+			this.regBefore(id, [postId], true);
+			this.regBefore(postId, this.tidy('before', pos));
 		}
 		return this;
 	}
 	protected loadImp(id: Id) {
 		if (--this.countMap[id]) return;
 		this.actMap[id]?.forEach(fn => this.n = fn(this.n));
-		this.postListMap[id]?.forEach(this.load);
+		this.postListMap[id]?.reverse().forEach(this.load);
 	}
 	load = (id: Id = Loader.START) => this.loadImp(id);
 }
--- packages/aocudeo/test/t0.ts
+++ packages/aocudeo/test/t0.ts
@@ -8,11 +8,12 @@ const loader = new Loader;
 
 const a = new Loader(1);
 
+loader.insert(9, { preOf: 0, after: 2 }, log(9))
+loader.insert(8, { postOf: 2 }, log(8))
 loader.insert(0, {}, log(0))
 loader.insert(1, {after:[0, 2]}, log(1))
 loader.insert(2, {}, log(2))
-loader.insert('z', {after:'pre:2'}, log('z'))
-loader.insert('z', {before:2,after:'pre:2'})
+loader.addAct(Loader.END, log('end'))
 
 a.addAct(0, (n) => {
 	return n;
