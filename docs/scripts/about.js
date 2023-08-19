549a00afbd688370a9617657c87e1a86f6be5ccd
E0SelmY4V <yabo1887415157@qq.com>
2023-08-19 17:08:11 +0800

    Add mcdjs, docs 架构图

--- docs/scripts/about.js
+++ docs/scripts/about.js
@@ -3,13 +3,22 @@
 "use strict";
 
 async function getUmlUrl(/**@type {string} */uml) {
-	const res = await fetch(uml);
-	const text = replace(await res.text(), [['\n', ';'], ['"', '\\"']]);
+	const res = await fetch(`${uml}?=${Math.random()}`);
+	const text = replace(await res.text(), [['\n', ';']]);
 	return `https://www.gravizo.com/svg?${text}`;
 }
 window.onload = async () => {
 	const [EdiagramList] = ['EdiagramList'].map(getN);
-	EdiagramList.innerHTML += `
-		<img src="${await getUmlUrl('/packages/aocudeo/lib/arch.puml')}" />
-	`;
+	/**@type {{[alt:string]:string}} */
+	const umls = {
+		'Aocudeo 架构图': '/packages/aocudeo/lib/arch.puml',
+		'McdJS 架构图': '/packages/mcdjs/lib/arch.puml',
+	};
+	for (const alt in umls) {
+		const Eimg = document.createElement('img');
+		Eimg.src = await getUmlUrl(umls[alt]);
+		Eimg.alt = alt;
+		EdiagramList.appendChild(Eimg);
+		EdiagramList.appendChild(document.createElement('br'));
+	}
 }
