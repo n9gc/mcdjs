504fe5eef536a5b7b04d314aac5fbfb4f14803b1
E0SelmY4V <yabo1887415157@qq.com>
2023-08-21 01:35:20 +0800

    Fix docs 匹配新版mcdjs

--- docs/scripts/playground.js
+++ docs/scripts/playground.js
@@ -4,14 +4,13 @@
 "use strict";
 
 window.onload = () => {
-	McdJS.appinf.globalify();
 	const [Ebutton, Einput, Eoutput] = ['Ebutton', 'Einput', 'Eoutput'].map(getN);
 	Ebutton.onclick = async () => {
 		const result = await McdJS.appinf.parse('playground', () => {
 			const Escript = document.createElement('script');
 			if ('value' in Einput && typeof Einput.value === 'string') Escript.innerHTML = Einput.value;
 			document.head.appendChild(Escript);
-		});
+		}, { globalify: true });
 		Eoutput.innerHTML = JSON.stringify(result, null, '  ');
 	};
 };
