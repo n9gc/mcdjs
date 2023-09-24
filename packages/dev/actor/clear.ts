9a91506e98b5d52c8776cf6016647b36e7a5c7b0
E0SelmY4V <yabo1887415157@qq.com>
2023-09-24 16:15:10 +0800

    Add devok, mcdjs 更完善的开发流程

--- packages/dev/actor/clear.ts
+++ packages/dev/actor/clear.ts
@@ -1,7 +1,7 @@
 /**
  * 清理目录
  * @module @mcdjs/dev/tester/all
- * @version 1.0.1
+ * @version 1.1.0
  * @license GPL-2.0-or-later
  */
 declare module './clear';
@@ -10,6 +10,7 @@ import Initer from 'lethal-build';
 import * as path from 'path';
 import checkrun from '../tool/checkrun';
 
+const lb = Initer(path.resolve());
 const {
 	dir,
 	snake,
@@ -18,9 +19,11 @@ const {
 	timeEnd,
 	time,
 	log,
-} = Initer(path.resolve());
+	ignoreList,
+} = lb;
 
-export default function def() {
+export default function def(ignores: string[]) {
+	lb.ignoreList = [...ignores, ...ignoreList];
 	return snake(
 		dels(RegExp(`${goodReg(dir + path.sep)}.*(\\.d\\.ts|\\.js|\\.js\\.map)$`)),
 		timeEnd(),
